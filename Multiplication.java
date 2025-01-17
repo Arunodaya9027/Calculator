b9e927505c496223c8112b2b945c84582428f5e0
Arunodaya9027 <arunodaya9027p.sg@gmail.com>
2025-01-17 13:57:48 +0530

    Added: Multiplication Calculator

--- Multiplication.java
+++ Multiplication.java
@@ -10,6 +10,6 @@ public class Multiplication {
         System.out.print("Enter Your third Number: ");
         int num3 = sc.nextInt();
 
-        System.out.print("Your multiplication of 3 numbers is: " + (num1-num2-num3));
+        System.out.print("Your multiplication of 3 numbers is: " + (num1*num2*num3));
     }
 }
